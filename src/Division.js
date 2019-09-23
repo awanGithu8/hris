10cafa480ccff4c961dec9805fa0ebd2e5ded256
Onesinus Santuy Parulian <onesinus231@gmail.com>
2019-09-23 09:05:25 +0700

    MOdule yang dibutuhkan kurang lebih udah ada mockupnya, tetapi untuk form belum dan filter juga belum incase sensitive

--- src/Division.js
+++ src/Division.js
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { Table, Input, Button, Row, Col, Divider } from 'antd';
 
 function Division() {
+  const [dataSource, setdataSource] = useState([]);
+
+  const data = [];
+  for (let i = 0; i < 25; i++) {
+    data.push({
+      key: i,
+      description: 'R&D'
+    });
+  }
+
+  useEffect(() => {
+    setdataSource(data);
+  }, [])
+
+  const columns = [
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description',
+      sorter: (a, b) => a.description.length - b.description.length,
+    },
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      key: 'action',
+      render: (text, record) => (
+        <span>
+          <a>Edit</a>
+          <Divider type="vertical" />
+          <a>Delete</a>
+        </span>
+      )
+    },
+  ];
+
+  function searchData(e){
+    let dataFilter = data.filter(function(d){
+      return (
+        d.name.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    })
+    setdataSource(dataFilter);
+  }
+
   return (
-    <div>Division tet</div>
+    <React.Fragment>
+      <center><h3>Division List</h3></center>
+      <Row>
+        <Col span={22}>
+          <Input 
+              placeholder="Find Division" 
+              onPressEnter={searchData}
+            />
+        </Col>
+        <Col span={2}>
+          <Button type="primary" icon="search">Search</Button>
+        </Col>
+      </Row>
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        pagination={{ defaultPageSize: 8, showSizeChanger: false }}
+
+      />
+    </React.Fragment>
   );
 }
 
--- src/User.js
+++ src/User.js
@@ -1,8 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { Table, Input, Button, Row, Col, Divider } from 'antd';
 
 function User() {
+  const [dataSource, setdataSource] = useState([]);
+
+  const data = [];
+  for (let i = 0; i < 25; i++) {
+    data.push({
+      key: i,
+      username: 'John ' + i,
+      role: 'Administrator'
+    });
+  }
+
+  useEffect(() => {
+    setdataSource(data);
+  }, [])
+
+  const columns = [
+    {
+      title: 'Username',
+      dataIndex: 'username',
+      key: 'username',
+      sorter: (a, b) => a.username.length - b.username.length,
+    },
+    {
+      title: 'Role',
+      dataIndex: 'role',
+      key: 'role',
+    },
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      key: 'action',
+      render: (text, record) => (
+        <span>
+          <a>Edit</a>
+          <Divider type="vertical" />
+          <a>Delete</a>
+        </span>
+      )
+    },
+  ];
+
+  function searchData(e){
+    let dataFilter = data.filter(function(d){
+      return (
+        d.name.includes(e.target.value)
+      )
+    })
+    setdataSource(dataFilter);
+  }
+
   return (
-    <div>User tet</div>
+    <React.Fragment>
+      <center><h3>User List</h3></center>
+      <Row>
+        <Col span={22}>
+          <Input 
+              placeholder="Find User" 
+              onPressEnter={searchData}
+            />
+        </Col>
+        <Col span={2}>
+          <Button type="primary" icon="search">Search</Button>
+        </Col>
+      </Row>
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        pagination={{ defaultPageSize: 8, showSizeChanger: false }}
+
+      />
+    </React.Fragment>
   );
 }
 
