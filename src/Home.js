9160907ac5bb29407a470f3c761208d6fa668c3f
Onesinus SPT <onesinus231@gmail.com>
2019-10-09 00:23:17 +0700

    Sing penting menu udah bisa dibagi lah cok

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
+import {UserContext} from "./context/UserContext";
 
 export default function Home(){
+    const user = useContext(UserContext);
     return (
         <div>
             <center>Santuy Human Resources Information System</center> 
             <br/><br/>
-            Welcome %username%,
+            Welcome {user.datauser.name}
         </div>    
     );
 }
--- src/Layout.js
+++ src/Layout.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Menu, Icon, Row, Col, Button } from 'antd';
+import { Menu, Icon, Row, Col, Button, message } from 'antd';
 import logo from './logo.svg';
 
 import { NavLink } from "react-router-dom";
@@ -10,13 +10,14 @@ import axios from 'axios';
 
 import { BACKEND_URL } from "./config/connection";
 
-import {UserContext} from "./context/UserContext";
+import { UserContext } from "./context/UserContext";
 
 const { SubMenu } = Menu;
 
 
 function Layout(props) {
   const user = useContext(UserContext);
+  const { role, isLogin, username } = user.datauser ? user.datauser : { role: "", isLogin: false, username: "" };
   console.log(user);
 
   // const [collapsed, setcollapsed] = useState(false);
@@ -36,22 +37,34 @@ function Layout(props) {
   //     );
   // }
 
+  function removeHashFromUrl() {
+    var loc = window.location.href,
+      index = loc.indexOf('#');
+
+    if (index > 0) {
+      window.location = loc.substring(0, index);
+    }
+  }
+
   function onClickLogout() {
-    axios.post(BACKEND_URL + 'userLoggedOut', { username: "Onesinus" });
-    setTimeout(
-      function () {
-        window.location.reload();
-      }
-        .bind(this),
-      1000
-    );
+    axios.post(BACKEND_URL + 'userLoggedOut', { username: username }).then(response => {
+      user.setuser([]);
+      // removeHashFromUrl()
+    }).catch(err => {
+      console.log(err);
+    });
+
   }
 
   // useEffect(() => {
   //   getAuth();
   // }, [])
 
-  if (!user.datauser.isLogin) {
+  if (!isLogin) {
+    if (user.datauser == null) {
+      message.info("Invalid Username or Password");
+    }
+
     return (
       <Login
 
@@ -75,53 +88,84 @@ function Layout(props) {
             defaultOpenKeys={['sub1', 'sub2', 'sub3']}
             mode="inline"
             theme="dark"
-            // inlineCollapsed={collapsed}
+          // inlineCollapsed={collapsed}
           >
 
             {/* <Button type="primary" onClick={toggleCollapsed} style={{ float: "right", marginBottom: "1vh" }}>
                     <Icon type={collapsed ? 'menu-unfold' : 'menu-fold'} />
                   </Button> */}
 
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <Icon type="hdd" />
-                  <span>Master</span>
-                </span>
-              }
-            >
-              <Menu.Item key="user"><NavLink to="/user"><Icon type="usergroup-add" /> User</NavLink></Menu.Item>
-              <Menu.Item key="division"><NavLink to="/division"><Icon type="apartment" /> Division</NavLink></Menu.Item>
-              <Menu.Item key="job_title"><NavLink to="/job_title"><Icon type="file-done" /> Job Title</NavLink></Menu.Item>
-              {/* <Menu.Item key="employee"><NavLink to="/employee">Employee</NavLink></Menu.Item> */}
-            </SubMenu>
+            {
+              role == "Administrator" &&
+              <SubMenu
+                key="sub1"
+                title={
+                  <span>
+                    <Icon type="hdd" />
+                    <span>Master</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="user"><NavLink to="/user"><Icon type="usergroup-add" /> User</NavLink></Menu.Item>
+                <Menu.Item key="division"><NavLink to="/division"><Icon type="apartment" /> Division</NavLink></Menu.Item>
+                <Menu.Item key="job_title"><NavLink to="/job_title"><Icon type="file-done" /> Job Title</NavLink></Menu.Item>
+                {/* <Menu.Item key="employee"><NavLink to="/employee">Employee</NavLink></Menu.Item> */}
+              </SubMenu>
+            }
 
-            <SubMenu
-              key="sub2"
-              title={
-                <span>
-                  <Icon type="table" />
-                  <span>Modules</span>
-                </span>
-              }
-            >
-              <Menu.Item key="daftar_cuti"><NavLink to="/cuti"><Icon type="menu" /> Permit List</NavLink></Menu.Item>
-              <Menu.Item key="pengajuan_cuti"><NavLink to="/add_cuti"><Icon type="form" /> Permit Request</NavLink></Menu.Item>
-              <Menu.Item key="approval"><NavLink to="/approval"><Icon type="check" /> Permit Approval</NavLink></Menu.Item>
-            </SubMenu>
+            {
+              ["Approver", "Administrator"].includes(role) &&
 
-            <SubMenu
-              key="sub3"
-              title={
-                <span>
-                  <Icon type="area-chart" />
-                  <span>Reports</span>
-                </span>
-              }
-            >
-              <Menu.Item key="report_permit"><NavLink to="/"><Icon type="pie-chart" /> Permit Report</NavLink></Menu.Item>
-            </SubMenu>
+
+              <SubMenu
+                key="sub2"
+                title={
+                  <span>
+                    <Icon type="table" />
+                    <span>Modules</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="daftar_cuti"><NavLink to="/cuti"><Icon type="menu" /> Permit List</NavLink></Menu.Item>
+                <Menu.Item key="pengajuan_cuti"><NavLink to="/add_cuti"><Icon type="form" /> Permit Request</NavLink></Menu.Item>
+                <Menu.Item key="approval"><NavLink to="/approval"><Icon type="check" /> Permit Approval</NavLink></Menu.Item>
+
+
+              </SubMenu>
+            }
+
+            {
+              role == "User" &&
+              <SubMenu
+                key="sub2"
+                title={
+                  <span>
+                    <Icon type="table" />
+                    <span>Modules</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="pengajuan_cuti"><NavLink to="/add_cuti"><Icon type="form" /> Permit Request</NavLink></Menu.Item>
+              </SubMenu>
+            }
+
+
+
+            {
+              role == "Administrator" &&
+              <SubMenu
+                key="sub3"
+                title={
+                  <span>
+                    <Icon type="area-chart" />
+                    <span>Reports</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="report_permit"><NavLink to="/"><Icon type="pie-chart" /> Permit Report</NavLink></Menu.Item>
+              </SubMenu>
+            }
+
             <Menu.Item key="logout"><a onClick={onClickLogout} ><Icon type="logout" /> Logout</a></Menu.Item>
           </Menu>
         </Col>
