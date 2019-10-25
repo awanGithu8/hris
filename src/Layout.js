a166ade00031c85143c0b2f06b1ca9e3c78deb11
Onesinus SPT <onesinus231@gmail.com>
2019-10-25 15:11:46 +0700

    Mantul

--- src/Layout.js
+++ src/Layout.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Menu, Icon, Row, Col, Button, message } from 'antd';
-import logo from './logo.svg';
+import React, { useState, useEffect, useContext } from "react";
+import { Menu, Icon, Row, Col, Button, message } from "antd";
 
 import { NavLink } from "react-router-dom";
 
-import Login from './Login';
+import Login from "./Login";
 
-import axios from 'axios';
+import axios from "axios";
 
 import { BACKEND_URL } from "./config/connection";
 
@@ -14,58 +13,26 @@ import { UserContext } from "./context/UserContext";
 
 const { SubMenu } = Menu;
 
-
 function Layout(props) {
   const user = useContext(UserContext);
-  let session_user = JSON.parse(window.localStorage.getItem('datauser'));
-  const { role, isLogin, username } = session_user ? session_user : { role: "", isLogin: false, username: "" };
-
-  // const [collapsed, setcollapsed] = useState(false);
-  // const [isLogin, setisLogin] = useState(false);
-
-  // function toggleCollapsed() {
-  //   setcollapsed(!collapsed)
-  // };
-
-  // function getAuth() {
-  //   axios.post(BACKEND_URL + 'checkUserLogin', { username: "onesinus", password: "123" })
-  //     .then((res) => {
-  //       if (res.data.data[0]) {
-  //         setisLogin(res.data.data[0].isLogin);
-  //       }
-  //     }
-  //     );
-  // }
-
-  function removeHashFromUrl() {
-    var loc = window.location.href,
-      index = loc.indexOf('#');
-
-    if (index > 0) {
-      window.location = loc.substring(0, index);
-    }
-  }
+  let session_user = JSON.parse(window.localStorage.getItem("datauser"));
+  const { role, username } = session_user
+    ? session_user
+    : { role: "", isLogin: false, username: "" };
 
   function onClickLogout() {
-    axios.post(BACKEND_URL + 'userLoggedOut', { username: username }).then(response => {
-      user.logoutuser();
-      // removeHashFromUrl()
-    }).catch(err => {
-      console.log(err);
-    });
-
+    axios
+      .post(BACKEND_URL + "userLoggedOut", { username: username })
+      .then(response => {
+        user.logoutuser();
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  // useEffect(() => {
-  //   getAuth();
-  // }, [])
-
   if (session_user == null) {
-    return (
-      <Login
-
-      />
-    )
+    return <Login />;
   } else {
     return (
       <Row>
@@ -76,24 +43,21 @@ function Layout(props) {
               src={"/sindata.png"}
               className="App-logo"
               alt="logo"
-              style={{ height: "25vh", width: "16.5vw", border: "15px solid #001529" }}
+              style={{
+                height: "25vh",
+                width: "16.5vw",
+                border: "15px solid #001529"
+              }}
             />
           </NavLink>
           <Menu
             style={{ height: "75vh", width: "16.5vw" }}
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1', 'sub2', 'sub3']}
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1", "sub2", "sub3"]}
             mode="inline"
             theme="dark"
-          // inlineCollapsed={collapsed}
           >
-
-            {/* <Button type="primary" onClick={toggleCollapsed} style={{ float: "right", marginBottom: "1vh" }}>
-                    <Icon type={collapsed ? 'menu-unfold' : 'menu-fold'} />
-                  </Button> */}
-
-            {
-              role == "Administrator" &&
+            {role == "Administrator" && (
               <SubMenu
                 key="sub1"
                 title={
@@ -103,17 +67,25 @@ function Layout(props) {
                   </span>
                 }
               >
-                <Menu.Item key="user"><NavLink to="/user"><Icon type="usergroup-add" /> User</NavLink></Menu.Item>
-                <Menu.Item key="division"><NavLink to="/division"><Icon type="apartment" /> Division</NavLink></Menu.Item>
-                <Menu.Item key="job_title"><NavLink to="/job_title"><Icon type="file-done" /> Job Title</NavLink></Menu.Item>
-                {/* <Menu.Item key="employee"><NavLink to="/employee">Employee</NavLink></Menu.Item> */}
+                <Menu.Item key="division">
+                  <NavLink to="/division">
+                    <Icon type="apartment" /> Division
+                  </NavLink>
+                </Menu.Item>
+                <Menu.Item key="job_title">
+                  <NavLink to="/job_title">
+                    <Icon type="file-done" /> Job Title
+                  </NavLink>
+                </Menu.Item>
+                <Menu.Item key="user">
+                  <NavLink to="/user">
+                    <Icon type="usergroup-add" /> User
+                  </NavLink>
+                </Menu.Item>
               </SubMenu>
-            }
+            )}
 
-            {
-              ["Approver", "Administrator"].includes(role) &&
-
-
+            {["Approver", "Administrator"].includes(role) && (
               <SubMenu
                 key="sub2"
                 title={
@@ -123,16 +95,25 @@ function Layout(props) {
                   </span>
                 }
               >
-                <Menu.Item key="daftar_cuti"><NavLink to="/cuti"><Icon type="menu" /> Permit List</NavLink></Menu.Item>
-                <Menu.Item key="pengajuan_cuti"><NavLink to="/add_cuti"><Icon type="form" /> Permit Request</NavLink></Menu.Item>
-                <Menu.Item key="approval"><NavLink to="/approval"><Icon type="check" /> Permit Approval</NavLink></Menu.Item>
-
-
+                <Menu.Item key="daftar_cuti">
+                  <NavLink to="/cuti">
+                    <Icon type="menu" /> Permit List
+                  </NavLink>
+                </Menu.Item>
+                <Menu.Item key="pengajuan_cuti">
+                  <NavLink to="/add_cuti">
+                    <Icon type="form" /> Permit Request
+                  </NavLink>
+                </Menu.Item>
+                <Menu.Item key="approval">
+                  <NavLink to="/approval">
+                    <Icon type="check" /> Permit Approval
+                  </NavLink>
+                </Menu.Item>
               </SubMenu>
-            }
+            )}
 
-            {
-              role == "User" &&
+            {role == "User" && (
               <SubMenu
                 key="sub2"
                 title={
@@ -142,14 +123,15 @@ function Layout(props) {
                   </span>
                 }
               >
-                <Menu.Item key="pengajuan_cuti"><NavLink to="/add_cuti"><Icon type="form" /> Permit Request</NavLink></Menu.Item>
+                <Menu.Item key="pengajuan_cuti">
+                  <NavLink to="/add_cuti">
+                    <Icon type="form" /> Permit Request
+                  </NavLink>
+                </Menu.Item>
               </SubMenu>
-            }
+            )}
 
-
-
-            {
-              role == "Administrator" &&
+            {role == "Administrator" && (
               <SubMenu
                 key="sub3"
                 title={
@@ -159,17 +141,23 @@ function Layout(props) {
                   </span>
                 }
               >
-                <Menu.Item key="report_permit"><NavLink to="/"><Icon type="pie-chart" /> Permit Report</NavLink></Menu.Item>
+                <Menu.Item key="report_permit">
+                  <NavLink to="/">
+                    <Icon type="pie-chart" /> Permit Report
+                  </NavLink>
+                </Menu.Item>
               </SubMenu>
-            }
+            )}
 
-            <Menu.Item key="logout"><a onClick={onClickLogout} ><Icon type="logout" /> Logout</a></Menu.Item>
+            <Menu.Item key="logout">
+              <a onClick={onClickLogout}>
+                <Icon type="logout" /> Logout
+              </a>
+            </Menu.Item>
           </Menu>
         </Col>
         <Col span={20}>
-          <div className="content">
-            {props.children}
-          </div>
+          <div className="content">{props.children}</div>
         </Col>
       </Row>
     );
--- src/User.js
+++ src/User.js
@@ -1,67 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { Table, Input, Button, Row, Col, Divider, Icon, Tooltip, Modal, Form, Select } from 'antd';
+import {
+  Table,
+  Input,
+  Button,
+  Row,
+  Col,
+  Divider,
+  Icon,
+  Tooltip,
+  Modal,
+  Form,
+  Select
+} from "antd";
 
-import axios from 'axios';
+import axios from "axios";
 
-import {BACKEND_URL} from "./config/connection";
-
+import { BACKEND_URL } from "./config/connection";
 
 const { Option } = Select;
 const { confirm } = Modal;
 
-function User({
-  form
-}) {
+function User({ form }) {
   const [dataSource, setdataSource] = useState([]);
   const [ModalUserVisible, setModalUserVisible] = useState(false);
   const [modalTitle, setModalTitle] = useState("");
   const [modalData, setModalData] = useState("");
   const [data, setData] = useState([]);
-  const [dataJobTitle,setdataJobTitle] = useState([]);
-  const [dataJobTitleDivision,setDataJobTitleDivision] = useState([]);
+  const [dataJobTitle, setdataJobTitle] = useState([]);
+  const [dataJobTitleDivision, setDataJobTitleDivision] = useState([]);
 
-  const [firstLoad, setFirstLoad] = useState(true)
+  const [firstLoad, setFirstLoad] = useState(true);
 
   const columns = [
     {
-      title: 'Username',
-      dataIndex: 'username',
-      key: 'username',
-      sorter: (a, b) => a.username.length - b.username.length,
+      title: "Username",
+      dataIndex: "username",
+      key: "username",
+      sorter: (a, b) => a.username.length - b.username.length
     },
     {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      sorter: (a, b) => a.name.length - b.name.length,
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      sorter: (a, b) => a.name.length - b.name.length
     },
     {
-      title: 'Division',
-      dataIndex: 'division',
-      key: 'division',
+      title: "Division",
+      dataIndex: "division",
+      key: "division"
     },
     {
-      title: 'Job Title',
-      dataIndex: 'job_title',
-      key: 'job_title',
+      title: "Job Title",
+      dataIndex: "job_title",
+      key: "job_title"
     },
     {
-      title: 'Remaining Permit',
-      dataIndex: 'remaining',
-      key: 'remaining',
+      title: "Remaining Permit",
+      dataIndex: "remaining",
+      key: "remaining"
     },
 
     {
-      title: 'Role',
-      dataIndex: 'role',
-      key: 'role',
-      sorter: (a, b) => a.role.length - b.role.length,
+      title: "Role",
+      dataIndex: "role",
+      key: "role",
+      sorter: (a, b) => a.role.length - b.role.length
     },
     {
-      title: 'Action',
-      dataIndex: 'action',
-      key: 'action',
+      title: "Action",
+      dataIndex: "action",
+      key: "action",
       render: (text, record) => (
         <span>
           <Tooltip title="Edit User">
@@ -77,31 +86,25 @@ function User({
           </Tooltip>
         </span>
       )
-    },
+    }
   ];
 
-  
   useEffect(() => {
     refreshData();
     getJobTitle();
-  }, [])
+  }, []);
 
-  function refreshData(){
+  function refreshData() {
     setFirstLoad(true);
-    setTimeout(
-        function() {
-          axios.get(BACKEND_URL+'getData')
-            .then((res) => {
-              console.log(res);
-              setDataNeed(res.data.data)
-            }
-          )
-        },
-        1000
-    );
+    setTimeout(function() {
+      axios.get(BACKEND_URL + "getData").then(res => {
+        console.log(res);
+        setDataNeed(res.data.data);
+      });
+    }, 1000);
   }
-  
-  function setDataNeed(skiw){
+
+  function setDataNeed(skiw) {
     console.log(skiw);
     setdataSource(skiw);
     setData(skiw);
@@ -111,33 +114,33 @@ function User({
   function searchData(e) {
     console.log(e.target.value);
     console.log(data);
-    let dataFilter = data.filter(function (d) {
+    let dataFilter = data.filter(function(d) {
       return (
-        d.username.toLowerCase().includes(e.target.value.toLowerCase())
-        ||
+        d.username.toLowerCase().includes(e.target.value.toLowerCase()) ||
         d.role.toLowerCase().includes(e.target.value.toLowerCase())
-      )
-    })
+      );
+    });
     setdataSource(dataFilter);
   }
 
   function handleOk() {
     validateFieldsAndScroll((errors, values) => {
-      if(!errors){
-        if(modalData === ""){ // Add New Data
+      if (!errors) {
+        if (modalData === "") {
+          // Add New Data
           values.division = dataJobTitleDivision[values.job_title];
           console.log(values);
-          axios.post(BACKEND_URL+'putData', values);
-        }else{
-          axios.post(BACKEND_URL+'updateData', {
+          axios.post(BACKEND_URL + "putData", values);
+        } else {
+          axios.post(BACKEND_URL + "updateData", {
             id: modalData["_id"],
-            update: values,
+            update: values
           });
         }
         setModalData("");
         setModalUserVisible(false);
         refreshData();
-      }else{
+      } else {
         console.log(errors);
       }
     });
@@ -161,44 +164,48 @@ function User({
     setModalUserVisible(true);
   }
 
-  function onClickDelete(record){
+  function onClickDelete(record) {
     confirm({
-      title: 'Are you sure delete this user?',
-      content: 'When user deleted you can\'t get it back',
-      okText: 'Yes',
-      okType: 'danger',
-      cancelText: 'No',
+      title: "Are you sure delete this user?",
+      content: "When user deleted you can't get it back",
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
       onOk() {
-        axios.delete(BACKEND_URL+'deleteData', {
+        axios.delete(BACKEND_URL + "deleteData", {
           data: {
             id: record["_id"]
-          },
+          }
         });
         refreshData();
       },
       onCancel() {
-        console.log('Cancel');
-      },
+        console.log("Cancel");
+      }
     });
   }
 
   function getJobTitle() {
-    axios.get(BACKEND_URL+"listJobTitle").then(res => {
+    axios.get(BACKEND_URL + "listJobTitle").then(res => {
       let job_title = [];
       let job_title_division = [];
       for (const [index, value] of res.data.data.entries()) {
-        job_title.push(<Option key={index} value={value.description}>{value.description}</Option>)
+        job_title.push(
+          <Option key={index} value={value.description}>
+            {value.description}
+          </Option>
+        );
         job_title_division[value.description] = value.division;
       }
-  
+
       setdataJobTitle(job_title);
       setDataJobTitleDivision(job_title_division);
     });
   }
-  
+
   const { getFieldDecorator, validateFieldsAndScroll } = form;
 
-  return(
+  return (
     <React.Fragment>
       <Modal
         title={modalTitle}
@@ -209,52 +216,60 @@ function User({
       >
         <Form className="login-form">
           <Form.Item>
-            {getFieldDecorator('username', {
-              initialValue: modalData?modalData.username:"",
-              rules: [{ required: true, message: 'Please input your username!' }],
+            {getFieldDecorator("username", {
+              initialValue: modalData ? modalData.username : "",
+              rules: [
+                { required: true, message: "Please input your username!" }
+              ]
             })(
               <Input
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                prefix={
+                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
                 placeholder="Username"
-              />,
+              />
             )}
           </Form.Item>
+          {modalData.username && (
+            <Form.Item>
+              {getFieldDecorator("password", {
+                rules: [
+                  { required: true, message: "Please input your Password!" }
+                ]
+              })(
+                <Input
+                  prefix={
+                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  type="password"
+                  placeholder="Password"
+                />
+              )}
+            </Form.Item>
+          )}
           <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
+            {getFieldDecorator("name", {
+              initialValue: modalData ? modalData.name : "",
+              rules: [{ required: true, message: "Please input your name!" }]
             })(
               <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="Password"
-              />,
-            )}
-          </Form.Item>          
-          <Form.Item>
-            {getFieldDecorator('name', {
-              initialValue: modalData?modalData.name:"",
-              rules: [{ required: true, message: 'Please input your name!' }],
-            })(
-              <Input
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                prefix={
+                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
                 placeholder="Name"
-              />,
+              />
             )}
           </Form.Item>
           <Form.Item>
-            {getFieldDecorator('job_title', {
-               initialValue: modalData?modalData.job_title:undefined,
-              rules: [{ required: true, message: 'Please Select Job Title' }],
-            })(
-              <Select placeholder="Job Title">
-                {dataJobTitle}
-              </Select>
-            )}
+            {getFieldDecorator("job_title", {
+              initialValue: modalData ? modalData.job_title : undefined,
+              rules: [{ required: true, message: "Please Select Job Title" }]
+            })(<Select placeholder="Job Title">{dataJobTitle}</Select>)}
           </Form.Item>
           <Form.Item>
-            {getFieldDecorator('role', {
-               initialValue: modalData?modalData.role:undefined,
-              rules: [{ required: true, message: 'Please Select Role' }],
+            {getFieldDecorator("role", {
+              initialValue: modalData ? modalData.role : undefined,
+              rules: [{ required: true, message: "Please Select Role" }]
             })(
               <Select placeholder="Role">
                 <Option value="Administrator">Administrator</Option>
@@ -264,28 +279,33 @@ function User({
             )}
           </Form.Item>
           <Form.Item>
-            {getFieldDecorator('remaining', {
-              initialValue: modalData?modalData.remaining:"",
-              rules: [{ required: true, message: 'Please input Permit Remaining!' }],
+            {getFieldDecorator("remaining", {
+              initialValue: modalData ? modalData.remaining : "",
+              rules: [
+                { required: true, message: "Please input Permit Remaining!" }
+              ]
             })(
               <Input
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                prefix={
+                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
                 placeholder="Permit Remaining"
-              />,
+              />
             )}
-          </Form.Item>          
+          </Form.Item>
         </Form>
       </Modal>
-      <center><h3>User List</h3></center>
+      <center>
+        <h3>User List</h3>
+      </center>
       <Row>
         <Col span={22}>
-          <Input
-            placeholder="Find User"
-            onPressEnter={searchData}
-          />
+          <Input placeholder="Find User" onPressEnter={searchData} />
         </Col>
         <Col span={2}>
-          <Button type="primary" icon="search">Search</Button>
+          <Button type="primary" icon="search">
+            Search
+          </Button>
         </Col>
       </Row>
       <Button
@@ -300,13 +320,13 @@ function User({
         dataSource={dataSource}
         columns={columns}
         pagination={{ defaultPageSize: 6, showSizeChanger: false }}
-        rowKey="_id" 
+        rowKey="_id"
         loading={firstLoad}
       />
     </React.Fragment>
-    );
+  );
 }
 
-const UserForm = Form.create({ name: 'user' })(User);
+const UserForm = Form.create({ name: "user" })(User);
 
 export default UserForm;
