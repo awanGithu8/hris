925551c869c1fab7f1bc86c9df89f1bbb32df705
Onesinus SPT <onesinus231@gmail.com>
2019-11-01 15:17:02 +0700

    Crud API special leave

--- backend/server.js
+++ backend/server.js
@@ -338,6 +338,57 @@ router.post("/userLoggedOut", (req, res) => {
 });
 /* End Api Login */
 
+/* API Special Permit */
+
+router.get("/listSpecialPermit", (req, res) => {
+  SpecialLeave.find((err, data) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
+  }).sort({
+    description: 1
+  });
+});
+
+router.post("/addSpecialPermit", (req, res) => {
+  let data = new SpecialLeave();
+
+  for (var key in req.body) {
+    if (req.body.hasOwnProperty(key)) {
+      data[key] = req.body[key];
+    }
+  }
+
+  if (req.body.description == "" || req.body.permit_total == "") {
+    return res.json({
+      success: false,
+      error: "Description and Permit Total is required"
+    });
+  }
+
+  data.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+router.post("/updateSpecialPermit", (req, res) => {
+  const { id, update } = req.body;
+  SpecialLeave.findByIdAndUpdate(id, update, err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+router.delete("/deleteSpecialPermit", (req, res) => {
+  const { id } = req.body;
+  SpecialLeave.findByIdAndRemove(id, err => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+/* END API Special Permit */
+
+
 /* Clear All Data */
 router.get("/deleteAllData", (req, res) => {
   let arr_Tables = [Cuti, User, Division, JobTitle];
