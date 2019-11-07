9f469899caf44f7a4efad6e4422384e166ffed11
Onesinus SPT <onesinus231@gmail.com>
2019-11-07 12:35:21 +0700

    Deploy 2

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3001/api/";
-// export const BACKEND_URL = "https://hris-backend.herokuapp.com/api/";
+// export const BACKEND_URL = "http://localhost:3001/api/";
+export const BACKEND_URL = "https://hris-backend.herokuapp.com/api/";
