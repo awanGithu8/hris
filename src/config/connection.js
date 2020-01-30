d687a6fcf15532f9e8ff6b433e28c7b5a46961a0
Onesinus SPT <onesinus231@gmail.com>
2020-01-30 23:51:53 +0700

    Oke

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,2 +1,6 @@
-// export const BACKEND_URL = "http://localhost:3001/api/";
-export const BACKEND_URL = "https://hris-backend.herokuapp.com/api/";
+const backend_urls = {
+    live: "https://hris-backend.herokuapp.com/api/",
+    dev: "http://localhost:3001/api/"
+}
+
+export const BACKEND_URL = backend_urls.dev;
